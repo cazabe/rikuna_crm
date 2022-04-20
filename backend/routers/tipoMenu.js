bb6b89b1ae00c3bb7fdb43c7596de38b46a33930
Carlos Zavala <cazcarlos@hotmail.com>
2022-04-20 17:10:29 -0500

    Adding testing and mannaging authentification on controllers that need it

--- backend/routers/tipoMenu.js
+++ backend/routers/tipoMenu.js
@@ -10,7 +10,7 @@ const {
 } = require('../controllers/tipoMenu/tipoMenu')
 
 route.post('/api/tipo/menu', authToken, authRole(), createMenu);
-route.get('/api/tipo/menu', authToken, authRole(), getMenu);
+route.get('/api/tipo/menu', getMenu);
 route.get('/api/tipo/menu/:id', authToken, authRole(), getOneMenu);
 route.put('/api/tipo/menu/:id', authToken, authRole(), updateMenu);
 
